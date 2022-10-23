402705a7f09f6e87743801f559c73f4f2d4fb7f9
toni_craveiro <craveiroa@merrimack.edu>
2022-10-22 21:36:34 -0400

    GraphicsDeck and GraphicsCard BareBones
    Implementation is prototyped for now. Just testing how it works

--- Project/js/Deck.js
+++ Project/js/Deck.js
@@ -1,20 +1,39 @@
+import { Card } from "./card";
+
 /**
- * 
+ * This is the Deck Class. It stores an array of 0
+ * or more cards, and can be used to do basic card
+ * manipulations.
  */
 export class Deck {
 
+    /**
+     * Generates an empty Deck
+     */
     constructor() {
         this.cards = [];
     }
 
+    /**
+     * Returns size of the deck
+     * @returns number
+     */
     getSize() {
-        return cards.length;
+        return this.cards.length;
     }
 
+    /**
+     * Boolean on if the deck is empty
+     * @returns true if deckSize = 0, false otherwise
+     */
     isEmpty() {
         return (this.cards.length == 0)
     }
 
+    /**
+     * Reveals the top card without removing it from the deck
+     * @returns {Card} Card
+     */
     peekTop() {
         if (!this.isEmpty())
             return cards[this.cards.length - 1];
@@ -24,6 +43,10 @@ export class Deck {
         return undefined;
     }
 
+    /**
+     * Reveals the bottom card without removing it from the deck
+     * @returns {Card} Card
+     */
     peekBottom() {
         if (!this.isEmpty())
             return cards[0];
@@ -33,6 +56,10 @@ export class Deck {
         return undefined;
     }
 
+    /**
+     * Removes the top card from the deck and returns it
+     * @returns {Card} Card
+     */
     takeTop() {
         if (!this.isEmpty())
             return this.cards.pop();
@@ -41,6 +68,10 @@ export class Deck {
         return undefined;
     }
 
+    /**
+     * Removes the bottom card from the deck and returns it
+     * @returns {Card} Card
+     */
     takeBottom() {
         if (!this.isEmpty())
             return this.cards.shift();
@@ -49,20 +80,45 @@ export class Deck {
         return undefined;
     }
 
+    /**
+     * Adds a card to the top of the Deck
+     * @param {Card} card
+     */
     addTop(card) {
         this.cards.push(card);
     }
 
+    /**
+     * Adds a card to the bottom of the Deck
+     * @param {Card} card 
+     */
     addBottom(card) {
         this.cards.unshift(card);
     }
 
+    /**
+     * peeks at the card specified by the index
+     * @param {number} idx 
+     */
+    lookAt(idx) {
+        return this.cards[idx];
+    }
+
+    /**
+     * Swaps the positions of two cards, given the position
+     * in the Deck.
+     * @param {number} p1 index of first card
+     * @param {number} p2 index of second card
+     */
     swap(p1, p2) {
         let temp = this.cards[p1];
         this.cards[p1] = this.cards[p2];
         this.cards[p2] = temp;
     }
 
+    /**
+     * Shuffles the deck
+     */
     shuffle() {
         for (let i = 0; i < this.cards.length; i++)
         {
--- Project/js/main.js
+++ Project/js/main.js
@@ -1,13 +1,24 @@
 import '../style.css';
 import * as THREE from 'three';
+import * as TWEEN from '@tweenjs/tween.js'
 
 import {Deck} from './deck';
 import {StandardDeck} from './standardDeck';
+import { Mesh } from 'three';
 
 let scene, camera, renderer;
 
-let gameCanvas = document.getElementById('gameOfWar')
+let gameCanvas = document.getElementById('gameOfWar');
 
+let cardGeometry, cardMaterial;
+
+let myDeck;
+
+const CARD_THICKNESS = 0.00024;
+
+/**
+ * Startup Function
+ */
 function main() {
 
   loadAssets();
@@ -19,21 +30,72 @@ function main() {
 
 } //end of main
 
+/**
+ * Preloads all assets (textures and models)
+ */
 function loadAssets() {
 
+  cardGeometry = new THREE.BoxGeometry(0.0571, 0.0829, CARD_THICKNESS); //numbers come from real card
+
+  cardMaterial = new THREE.MeshStandardMaterial({
+    color:0xFFFFFF
+  });
 } //end of loadAssets
 
+/**
+ * Generates the model world we will project in
+ * the graphics world
+ */
 function initLogic() {
-  let myDeck = new StandardDeck();
-
-  console.log(myDeck);
-
+  myDeck = new StandardDeck();
+  
   myDeck.shuffle();
 
   console.log(myDeck);
 
 } //end of initLogic
 
+/**
+ * creates a card given a Card and a texture
+ */
+function createCardModel(card, texture) {
+  let mesh = new THREE.Mesh(cardGeometry, cardMaterial);
+  mesh.userData = card;
+  return mesh;
+} //end of createCardModel
+
+/**
+ * creates a deck, and generates cards if it contains them
+ * @param {Deck} deck deck to be projected into the world
+ */
+function createDeckModel(deck) {
+  let deckModel = new THREE.Group();
+
+  if(deck.isEmpty())
+    return deckModel;
+
+  let offset = 0;
+  for(let i = 0; i < deck.getSize(); i++)
+  {
+
+    let card = deck.lookAt(i);
+    let cardModel = createCardModel(card, undefined);
+
+    cardModel.position.z = offset;
+
+    deckModel.add(cardModel);
+
+    offset += CARD_THICKNESS;
+
+  } //end of for
+
+  return deckModel;
+
+} //end of createdeckModel 
+
+/**
+ * builds the view the user will see
+ */
 function initGraphics() {
 
   //Scene
@@ -45,15 +107,15 @@ function initGraphics() {
 
   camera = new THREE.PerspectiveCamera(90, gameCanvas.clientWidth/gameCanvas.clientHeight, 0.1, 1000);
   camera.name = 'camera';
-  camera.position.z = 0.5;
-  camera.position.y = 0.5;
+  camera.position.z = 0.2;
+  camera.position.y = 0.2;
   camera.lookAt(new THREE.Vector3(0,0,0));
 
   scene.add(camera);
 
   //Lighting
 
-  let ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.5);
+  let ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.1);
   scene.add(ambientLight);
 
   let pointLight = new THREE.PointLight(0xFFFFFF);
@@ -63,15 +125,20 @@ function initGraphics() {
 
   //Geometry
 
+  let deckModel = createDeckModel(myDeck)
+  deckModel.rotateX(Math.PI/2);
+  scene.add(deckModel);
+
   //let tableGeometry = new THREE.BoxGeometry(2, 0.05, 0.5);
 
   //Material
 
-  /*let tableMaterial = new THREE.MeshStandardMaterial({
+ /* let tableMaterial = new THREE.MeshStandardMaterial({
     color: 0xC04000
   });
 
   let table = new THREE.Mesh(tableGeometry, tableMaterial);
+  table.position.y -= .1;
   scene.add(table);
  */
 
@@ -87,16 +154,27 @@ function initGraphics() {
 
 } //end of initGraphics
 
+/**
+ * Creates the contoller for the user to interact
+ * with the view.
+ */
 function initController() {
 
 } //end of initController
 
+/**
+ * This is where the game and its events occur
+ */
 function gameLoop() {
 
   render();
 
 } //end of gameLoop
 
+/**
+ * A basic render method, in case special steps
+ * must be taken during a single render.
+ */
 function render() {
   renderer.render(scene, camera);
 } //end of render
